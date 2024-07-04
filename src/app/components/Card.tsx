ce4ec40053b2cfeb35c1e2881e294fac0db6ff92
Willian José Henkel de Deus <willian.deus@hitechnologies.com.br>
2024-07-04 18:16:13 -0300

    added animation

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,7 +1,39 @@
+'use client'
+
+import { motion, Variants } from "framer-motion";
+import { FaWhatsapp } from "react-icons/fa";
+
+const cardVariants: Variants = {
+  offscreen: {
+    y: 300
+  },
+  onscreen: {
+    y: 50,
+    transition: {
+      type: "spring",
+      bounce: 0.4,
+      duration: 0.8
+    }
+  }
+}
+
 export default function Card() {
   return (
-    <div className="w-80 h-96 bg-orange-300 rounded-md">
-
-    </div>
+    <motion.div
+      initial="offscreen"
+      whileInView="onscreen"
+    >
+      <motion.div variants={cardVariants}>
+      <div className="w-80 h-96 rounded-md p-6">
+        <div className="text-8xl flex justify-center">
+          <FaWhatsapp />
+        </div>
+        <div className="text-center">
+          <h2 className="text-2xl font-bold">Whatsapp</h2>
+          <h1 className="text-3xl">41 99999-9999</h1>
+        </div>
+      </div>
+      </motion.div>
+    </motion.div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,19 @@ import Card from "./components/Card";
 export default function Index() {
   return (
     <>
-      <section className="w-full h-[500px] px-4">
-        <div className="w-full h-full bg-[url('../../assets/teste.png')] bg-fixed rounded p-2 pr-8 animate-fade-right bg-no-repeat">
-          <div className="w-full h-full flex items-end justify-center flex-col animate-fade-up">
-            <div className="bg-white p-4 py-10 opacity-1 rounded-md text-right flex flex-col shadow-lg">
-              <h1 className="text-3xl text-black font-semibold">Empresa especializada em entregas de GLP</h1>
+      <section className="w-full h-[500px]">
+        <div className="w-full h-full bg-[url('../../assets/teste.png')] bg-fixed rounded p-2 pr-8 animate-fade-up bg-no-repeat bg-cover">
+          <div className="w-full h-full flex items-center justify-center flex-col animate-fade-right">
+            <div className="bg-white p-4 py-10 opacity-1 rounded-md text-center flex flex-col shadow-lg">
+              <h1 className="text-3xl text-black font-bold">Empresa especializada em entregas de GLP</h1>
               <p className="mt-2 text-gray-600">Trabalhamos sempre com o melhor atendimento</p>
             </div>
-            <button type="submit" className="text-white mt-5 bg-orange-0 hover:bg-orange-400 focus:ring-4 focus:outline-none focus:bg-orange-400 font-medium rounded-lg text-sm  sm:w-auto px-20 py-5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:bg-orange-600">Fazer pedido</button>
+            <button type="submit" className="text-white font-semibold mt-5 bg-orange-0 hover:bg-orange-400 focus:ring-4 focus:outline-none focus:bg-orange-400 rounded-lg text-sm sm:w-auto px-20 py-5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:bg-orange-600">Fazer pedido</button>
           </div>
         </div>
       </section>
-      <section className="mt-10">
+      <section className="mt-20">
+        <h1 className="text-center font-semibold text-2xl">Contate-nos</h1>
         <div className="flex gap-10 justify-center p-2">
           <Card />
           <Card />
