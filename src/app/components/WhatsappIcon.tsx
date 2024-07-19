1519e199f35cdb024bcb8981d4997ac3e1d38e3b
Willian José Henkel de Deus <willian.deus@hitechnologies.com.br>
2024-07-19 10:16:40 -0300

    fixed

--- src/app/components/WhatsappIcon.tsx
+++ src/app/components/WhatsappIcon.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function WhatsappIcon() {
   return (
     <div className="w-16 h-16 fixed bottom-6 left-6 z-50 cursor-pointer">
-      <Link href="https://api.whatsapp.com/send?phone=5541984095429&text=Ol%C3%A1,%20gostaria%20de%20informa%C3%A7%C3%B5es%20sobre%20os%20gases.">
+      <Link href="https://api.whatsapp.com/send?phone=5541984095429&text=Ol%C3%A1,%20gostaria%20de%20informa%C3%A7%C3%B5es%20sobre%20os%GLP.">
       <Image src={whatsappIcon} alt="whatsapp_icon" />
       </Link>
     </div>
