0c9aa1832fd51c394fe17b3354c04c814f9ae566
Willian José Henkel de Deus <willian.deus@hitechnologies.com.br>
2024-08-26 18:33:37 -0300

    aa

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,10 @@ import path from 'path';
 const nextConfig = {
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
   images: {
+    unoptimized: true,
     domains: ['flowbite.s3.amazonaws.com'],
   },
+  output: 'export',
   webpack: (config) => {
     config.resolve.alias['@components'] = path.join(process.cwd(), 'src/app/components');
     config.resolve.alias['@pages'] = path.join(process.cwd(), 'src/app/pages');
